725194834610cef66f946812e7aeaec523d8f6b0
Louis Maddox <louismmx@gmail.com>
2024-06-02 17:59:18 +0100

    chore: Initial commit

---
+++ pyo3_counter_example/src/lib.rs
@@ -0,0 +1,35 @@
+use pyo3::prelude::*;
+
+#[pyclass]
+struct Counter {
+    value: i32,
+}
+
+#[pymethods]
+impl Counter {
+    #[new]
+    fn new() -> Self {
+        Counter { value: 0 }
+    }
+
+    fn increment(&mut self) {
+        self.value += 1;
+    }
+
+    fn get_value(&self) -> i32 {
+        self.value
+    }
+}
+
+#[pyfunction]
+fn increment_counter(counter: &PyCell<Counter>) {
+    let mut counter = counter.borrow_mut();
+    counter.increment();
+}
+
+#[pymodule]
+fn pyo3_counter_example(py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_class::<Counter>()?;
+    m.add_function(wrap_pyfunction!(increment_counter, m)?)?;
+    Ok(())
+}
