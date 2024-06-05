b5571cdf44930738f9ec0653ee8b207c14cee40e
Louis Maddox <louismmx@gmail.com>
2024-06-05 09:34:31 +0100

    test: test reuse of same client

--- pyo3_client_example/tests/test_client.py
+++ pyo3_client_example/tests/test_client.py
@@ -3,15 +3,31 @@ from pytest import mark
 from pyo3_client_example import ReqwestClient
 
 
-@mark.parametrize("url,expected", [("https://jsonplaceholder.typicode.com/todos/1", """\
+@mark.parametrize("url, expected", [("https://jsonplaceholder.typicode.com/todos/",
+("""\
 {
   "userId": 1,
   "id": 1,
   "title": "delectus aut autem",
   "completed": false
 }\
-""")])
+""",
+"""\
+{
+  "userId": 1,
+  "id": 2,
+  "title": "quis ut nam facilis et officia qui",
+  "completed": false
+}\
+""",
+)
+)])
 def test_client_get(url, expected):
+    url1 = url + "1"
+    url2 = url + "2"
+    expected1, expected2 = expected
     client = ReqwestClient()
-    response = client.get(url)
-    assert response == expected
+    response1 = client.get(url1)
+    assert response1 == expected1
+    response2 = client.get(url2)
+    assert response2 == expected2
