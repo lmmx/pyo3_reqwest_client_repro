1fcb597b8652c8bb2be2ca267ce56113f398cd31
Louis Maddox <louismmx@gmail.com>
2024-06-15 10:27:19 +0100

    test: multiple sleeps (5000 times) in parallel

--- pyo3_sleep_example/tests/test_increment.py
+++ pyo3_sleep_example/tests/test_increment.py
@@ -1,8 +1,23 @@
 import pyo3_sleep_example
+
+from asyncio import gather
 from pytest import mark
+
 
 @mark.asyncio
 async def test_sleep(capfd):
     await pyo3_sleep_example.rust_sleep()
     captured = capfd.readouterr()
     assert captured.out == "hello from rust!\n"
+
+
+@mark.asyncio
+async def test_sleep_multi(capfd):
+    async def run_sleep_and_capture():
+        await pyo3_sleep_example.rust_sleep()
+
+    num_tasks = 5000
+    tasks = [run_sleep_and_capture() for _ in range(num_tasks)]
+    results = await gather(*tasks)
+    captured = capfd.readouterr()
+    assert captured.out == "hello from rust!\n" * num_tasks
