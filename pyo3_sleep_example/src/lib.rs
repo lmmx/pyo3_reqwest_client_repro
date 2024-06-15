21373c0fabab99f81a2459ec5369f4ab711af2d2
Louis Maddox <louismmx@gmail.com>
2024-06-15 10:18:22 +0100

    test: test the sleep properly in pytest and its printout

--- pyo3_sleep_example/src/lib.rs
+++ pyo3_sleep_example/src/lib.rs
@@ -2,33 +2,6 @@ use pyo3::prelude::*;
 use pyo3_asyncio_0_21::tokio::future_into_py;
 use tokio::time::sleep;
 
-#[pyclass]
-struct Counter {
-    value: i32,
-}
-
-#[pymethods]
-impl Counter {
-    #[new]
-    fn new() -> Self {
-        Counter { value: 0 }
-    }
-
-    fn increment(&mut self) {
-        self.value += 1;
-    }
-
-    fn get_value(&self) -> i32 {
-        self.value
-    }
-}
-
-#[pyfunction]
-fn increment_counter(counter: &Bound<Counter>) {
-    let mut counter = counter.borrow_mut();
-    counter.increment();
-}
-
 #[pyfunction]
 fn rust_sleep(py: Python) -> PyResult<Bound<'_, PyAny>> {
     future_into_py(py, async {
@@ -40,8 +13,6 @@ fn rust_sleep(py: Python) -> PyResult<Bound<'_, PyAny>> {
 
 #[pymodule]
 fn pyo3_sleep_example(_py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_class::<Counter>()?;
-    m.add_function(wrap_pyfunction!(increment_counter, m)?)?;
     m.add_function(wrap_pyfunction!(rust_sleep, m)?)?;
     Ok(())
 }
